14cd90db13ff718bd9927bd91491efe1d6dcf1a7
bryanr1992 <bryan.rivas05@gmail.com>
2021-07-11 19:51:04 -0400

    More refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Menu from './layout/Menu';
+import Header from './layout/Header';
+import './App.css';
 
 const App = () => {
-    return <div>Hello World</div>;
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Menu />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Menu.css';
+
+const Menu = () => {
+  return (
+    <div id="mySidenav" class="sidenav">
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Clients</a>
+      <a href="#">Contact</a>
+    </div>
+  );
+};
+
+export default Menu;
