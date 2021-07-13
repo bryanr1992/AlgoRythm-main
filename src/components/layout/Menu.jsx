54a2e66c9f4f82c2aa8fa1be3d53261e89dc9ed4
bryanr1992 <bryan.rivas05@gmail.com>
2021-07-13 17:46:30 -0400

    refactoring

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -6,15 +6,19 @@ const Menu = () => {
     <div id="mySidenav" class="sidenav">
       <a href="#">
         <img className="menu-icons" src="../assets/icon/book.svg"></img>
+        <p className="menu-text">Learn</p>
       </a>
       <a href="#">
         <img className="menu-icons" src="../assets/icon/trophy.svg"></img>
+        <p className="menu-text">Challenge</p>
       </a>
       <a href="#">
         <img className="menu-icons" src="../assets/icon/focus.svg"></img>
+        <p className="menu-text">Practice</p>
       </a>
       <a href="#">
         <img className="menu-icons" src="../assets/icon/notes.svg"></img>
+        <p className="menu-text">Notes</p>
       </a>
     </div>
   );
