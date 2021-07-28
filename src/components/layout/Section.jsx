c0a8deae308c06aec07780ecb999b10d3f5c5c83
bryanr1992 <bryan.rivas05@gmail.com>
2021-07-27 20:41:07 -0400

    adjust section component

--- src/components/layout/Section.jsx
+++ src/components/layout/Section.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Section.css';
 
-const Section = () => {
+const Section = ({ to, name }) => {
   return (
     <div className="section-container">
-      <div className="section-child">
-        <div className="grid">
-          <div className="section-inner-child">Stacks</div>
+      <div className="section-rectangle">
+        <div className="line"></div>
+        <div className="line-2"></div>
+        <div className="line-3"></div>
+        <div className="line-4"></div>
+        <div className="line-5"></div>
+        <div className="line-6"></div>
+        <div className="line-7"></div>
+        <div className="line-8"></div>
+        <div className="line-9"></div>
+        <div className="line-10"></div>
+        <div className="line-11"></div>
+        <div className="line-12"></div>
+        <div className="line-13"></div>
+        <div className="line-14"></div>
+        <div className="inner-rectangle">
+          <Link className="section-p" to={to}>{name}</Link>
         </div>
       </div>
     </div>
--- src/components/pages/Outline.jsx
+++ src/components/pages/Outline.jsx
@@ -1,33 +1,15 @@
 import React, { useEffect } from 'react';
-import './Outline.css';
 import { Link } from 'react-router-dom';
+import Section from '../layout/Section';
+import './Outline.css';
 
 const Outline = () => {
   const changeIndex = () => {};
 
-//   useEffect(() => {
-//     const changeIndex = () => {
-//       const element = document.querySelector('.main-container');
-//       element.style.zIndex= 1;
-//       //console.log(element.style.zIndex);
-//     };
-//     changeIndex();
-//   }, []);
-
   return (
     <React.Fragment>
-      <ol class="gradient-list">
-        <li>
-          <Link to="/stacks">Stacks</Link>
-        </li>
-        <li>Insert link.</li>
-        <li>Insert link</li>
-        <li>Insert link.</li>
-        <li>Insert link.</li>
-        <li>Insert link.</li>
-        <li>Insert link.</li>
-        <li>Insert link.</li>
-      </ol>
+      <Section to="/stacks" name="Stacks" />
+      <Section to="/queues" name="Queues" />
     </React.Fragment>
   );
 };
