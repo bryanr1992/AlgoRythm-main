695be8bfada4ce293fe95b20d432c3ec595ea694
bryanr1992 <bryan.rivas05@gmail.com>
2021-07-29 14:06:52 -0400

    add modal functionality

--- src/components/pages/ModalPage.jsx
+++ src/components/pages/ModalPage.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './ModalPage.css';
 
 const ModalPage = () => {
+  var modal;
+  var btn;
+  var span;
+
+  const ref = useRef();
+
+  useEffect(() => {
+    modal = document.getElementById('myModal');
+    btn = document.getElementById('myBtn');
+    span = document.getElementsByClassName('close')[0];
+
+    const onModalClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        modal.style.display = 'none';
+      }
+    };
+
+    document.body.addEventListener('click', onModalClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener('click', onModalClick, {
+        capture: true,
+      });
+    };
+  }, []);
+
+  const handleClick = () => {
+    modal.style.display = 'block';
+  };
+
+  const handleClose = () => {
+    modal.style.display = 'none';
+  };
+
   return (
     <React.Fragment>
-      <button id="myBtn" className="modal-btn">Open Modal</button>
-      <div id="myModal" className="modal">
+      <button onClick={handleClick} id="myBtn" className="modal-btn">
+        Open Modal
+      </button>
+      <div ref={ref} id="myModal" className="modal">
         <div className="modal-content">
-          <span className="close">&times;</span>
+          <span onClick={handleClose} className="close">
+            &times;
+          </span>
           <p>Some text in the Modal..</p>
         </div>
       </div>
